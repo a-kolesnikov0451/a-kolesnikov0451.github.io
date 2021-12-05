9464d2266b20d85a96d0d316bfb1620dc716524e
pocketbeast-hash1 <godlike.skills420@gmail.com>
2021-12-05 22:02:06 +0300

    bait

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// получение ip
+let ip;
+$.getJSON('http://www.geoplugin.net/json.gp?jsoncallback=?', function(data) {
+        ip = data['geoplugin_request'];
+});
+
+// расположение враппера по центру
+function marginWrapper() {
+    const wrapper = document.getElementById('wrapper');
+
+    const wrapperHeight = document.getElementById('wrapper').clientHeight;
+    const screenHeight = document.documentElement.clientHeight;
+
+    const marginValue = (screenHeight / 2) - (wrapperHeight / 2) - 50;
+
+    wrapper.style.marginTop = marginValue + 'px';
+};
+marginWrapper();
+
+// кнопка
+const button = document.getElementById('button');
+button.addEventListener('click', changeText);
+
+function changeText() {
+    const text = document.querySelector('.text');
+    if (ip === "94.25.165.9"){
+        text.innerHTML = "Ты красавчик."
+    } else {
+        text.innerHTML = "Ты петух."
+    };
+    marginWrapper();
+};
